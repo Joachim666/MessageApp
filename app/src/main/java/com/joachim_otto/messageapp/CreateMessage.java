835ea01530456ac415bc38aafc30a4da2f933b2c
Joachim <joachimotto@wp.pl>
2017-02-23 14:37:40 +0100

    change

--- app/src/main/java/com/joachim_otto/messageapp/CreateMessage.java
+++ app/src/main/java/com/joachim_otto/messageapp/CreateMessage.java
@@ -1,7 +1,9 @@
 package com.joachim_otto.messageapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class CreateMessage extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class CreateMessage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_message);
+    }
+
+    public void onSendMessage(View view){
+        Intent intent = new Intent(this,ReciveMessage.class);
+        startActivity(intent);
+
     }
 }
---
+++ app/src/main/java/com/joachim_otto/messageapp/ReciveMessage.java
@@ -0,0 +1,13 @@
+package com.joachim_otto.messageapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReciveMessage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recive_message);
+    }
+}
