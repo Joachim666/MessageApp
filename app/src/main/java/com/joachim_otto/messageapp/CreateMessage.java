de66550a6a30c79340c518ac7d5d7469a7b5c23d
Joachim <joachimotto@wp.pl>
2017-02-23 13:16:24 +0100

    Initial commit

---
+++ app/src/main/java/com/joachim_otto/messageapp/CreateMessage.java
@@ -0,0 +1,13 @@
+package com.joachim_otto.messageapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateMessage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_message);
+    }
+}
